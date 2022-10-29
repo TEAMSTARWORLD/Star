2604667a98d0c8ab1a4b631aadfbfc9a50b2733b
тєαм ѕтαя <115808731+TEAMSTARWORLD@users.noreply.github.com>
2022-10-29 18:35:43 +0530

    Add files via upload

---
+++ star.py
@@ -0,0 +1,116 @@
+o
+
+}c�G�@s�ddlZddlZddlmZddlZddlZddlZddlZddlZddlTddl	m
+Z
+mZddlm
+Z
+mZddlmZddlmZmZddlmZmZmZmZdd	lmZmZmZmZdd
+lmZmZddl m!Z!m"Z"ddlm#Z#dd
+lm$Z$ej%�&d�s�e�'d�zddl(m)Z)ddlZWne�*d�e�*d�Ye
+�ej+Z,ej-Z.ej/Z0ej+Z1ej2Z3dZ4ej5Z6ej7Z8e.e0e3e8e6gZ9e0de3de0de1Z:e0de.de0de1Z;e3de0de3de1Z<e0de6de0de1Z=e3de0de3de1Z>e3de0de3de1Z?dZ@dZAdZBzddlCZCWneD�y7eEe0�de,���e�*d�Ynwd d!�ZFd"d#�ZGd$d%�ZH	eH�eG�eF�eEe0d'e,�eEe0d(e,�eEe0d)e,�eEe0d*e,�eEe0d+e,�eEe0d,e,�eIeJd-��ZKeKd.k�r!gZLeMd/d0���ZNeIeJd1e0�d2e.����ZOePeO�D]"ZQeReJd1e0�d3e.����ZSd4�TeS�U��ZVe�WeVgeN�eL�XeV��q�eEd1e0�d5��eH�eEd1e0�d6��eLD]-ZYed7eY��d8d9�ZZeZ�[eY�eZe#d:��e�d;�eZe#d<��eEe0�d=��eZ�\��q�eJd>�Wd�n	1�swYeN�]��nVeKd;k�r�gZ^eMd/d?�Z_	z
+e^�Xe�`e_��Wn
+ea�yBYnw�q.eEd1e:e0d@e1�e^D]rZKeKdZbeEe>�e4�dAe0�eb���ed7eb��d8d9�Zcec�d�gZeec�f��s�zec�geb�eEdB�Wn e�y�eEe;�dCe3�eb�dCe.�dDe1���ee�XeK�YnweeD]Zhe^�ieh�eEe:e0dEe1��q�e�dF�ec�\��qSeEe:dG�eH�eG�dHdI�ZjdJdK�ZkzceMdLd?��SZ_e�`e_�Zle_�]�eJe=�e6�dMe3�eld�e0�dNe.���ZmdOemv�reldZneIeld.�ZonejpdPk�re�*dQ�ne�*dR�eJe=�e6�dSe.���ZndZoWd�n	1�s7wYWneJe=�e6�dSe.���ZndZoYgZ^eMd/d?�Z_	z
+e^�Xe�`e_��Wn
+ea�ykYnw�qWeEe:�e0�dTe3�eqe^����eIeJe=�e6�dUe.����ZreEe:�e6�dVe0���eEe6�dWe0�dX��eEe6�dYe0�dZ��eIeJe=�e6�d[e.����Zsesdk�r�eReJe=�e6�d\e.����Ztn
+eReJe=�e6�d]e.����ZteEe4�d^�d_�d`da�e^der�D�ZueuD]Zve^�iev��q�eMd/db��$Z_e^D]	ZKe�WeKe_��q�euD]	Zwe�Wewe_��q	e_�]�Wd�n	1�s"wYeIeJe=�e6�dce3�de0�dde3�dee.��
+��ZxeEe<�e0�dfe3�eqeu��e0�dg��dZydZzeuD�]JZ{eodhZ|ed7e{d��d8d9�ZZeEe>�e4�die6�e{d�e0�dje6�dk�	�eZ�[e{d�eZ�}�j~Zz�dlenv�r�en�Udl�d.Z�zeZe!e���eEe>�e4�die6�e�e0�dm��Wn e�y�YnweZe#en��eEe>�e4�die6�e�e0�dm��eZ��en�Z�esdk�r�eZe#et��eEe>�e4�die6�e�e0�dn��eZ��et�Z�ee�j�e�j��Z�n1zet�Udl�d.Z�eZe!e���eEe>�e4�die6�e�e0�dn��Wn
+e�y$YnweZ��et�Z�e�Z�Wn/e��y\Z�z"eEe;�e.�die6�e�e0�do��eEe;�dCe.�e����WYdZ�[��qUdZ�[�wweEe>�e4�die6�e�e0�dje6�dp�	�z;gZ�d&Z�ed4�Z�dZ�d.ZQe��r�eZeene�e�dqddr��Z�e���e�j��e�eqe�j��7Z�e�e�j�7Z�eqe�j��d.k�r�dsZ�e��s�Wn(e��y�Z�zeEe;�e.�dt��eEe;�e.�dCe����WYdZ�[��qUdZ�[�wweqe��Zzezdk�s�J�eoezk�r�eEe;�e0�du���qUeEe:�e0�dve3�eo���dZ�e�eoe|�D�]�Z�eod.7Zoe�dwk�reEe;�e.�dx���n�z]esdk�r+eZee�e�g��n	eZe"e�j�e�dy��e�j~Z�e�j�Z�eEe>�e4�die6�e�e0�dje6�e��dCe0�dze6�e����eyd.7ZyeEe:�e4�die6�e�e0�d{e3�ex�dCe0�d|��e�ex�W�qe�y�eEe?�e4�die6�e�e0�dje.�d}�	�Y�qe�y�eEe;�e4�die6�e�e0�dje.�d~�	�e�d.7Z�Y�qe�y�eEe;�e.�d��eoezk�r�ejeneo�ek�Y�qe�y�eEe;�e4�die6�e�e0�dje.�d��	�Yn�e�yeEe;�e4�die6�e�e0�dje.�d��	�Y�qe�y/eEe?�e4�die6�e�e0�dje.�d��	�Y�qe�yMZ�zeEe;�e.�dCe����WYdZ�[�nVdZ�[�we��y`eEe;�e.�d���Y�qe��y�eEe;�e.�d���eoeqe��k�r|ejeneo�ek�Y�qe��y�Z�zeEe;�dCe����WYdZ�[��qdZ�[�ww�qUeydk�r�eEd1e:�e0�d���zeoezk�r�ejeneo�ek�W�n�ek�Y�n�eKd�k�r�gZ^gZ�eMd/d?�Z�	z
+e^�Xe�`e���Wn
+ea�y�Ynw�q�e��]�eqe^�dk�reEe.d��ed���npe^D]LZ�eRe�d�Z�ed7e���d8d9�Z�e��d�e��f��sTze��ge��eEe0�d�e��d�e,���W�q	e�ySeEe.eRe��d�e,�e��Xe��Y�q	w�q	eqe��dk�rieEe0d��eJd���ne�D]Z�e^�ie���qkeMd/db��Z�e^D]ZKeKdZ�e�We�ge���q|Wd�n	1�s�wYe��]�eEe0d�e,�eJd��n�eKd�k�	rMgZ�eMd/d?�Z_	z
+e��Xe�`e_��Wn
+ea�y�Ynw�q�e_�]�dZQeEe0�d���e�D]Z{eEe0�deQ�d�e{d�e,���eQd.7ZQ�q�eIeJd1e0�d�e,����ZoeRe�eod�Z�e�d�Z�ejpdPk�	re�*d�e����ne�*d�e����e�eo=eMd/db�Z_e�D]	Z�e�We�e_��	q0eEd1e0�d�e,���eJd��e_�]�n*eKd�k�	rid�Z�d�Z�e�e�Z�e�*e��eEd�e�e��d�d��neKd�k�	rweH�eG�e���qE)��N)�sleep)�*)�Key�Helpers)�init�Fore)�TelegramClient)�InputPeerChannel�ChannelParticipantsSearch)�PeerFloodError�UserPrivacyRestrictedError�PhoneNumberBannedError�ChatAdminRequiredError)�ChatWriteForbiddenError�UserBannedInChannelError�UserAlreadyParticipantError�FloodWaitError)�InviteToChannelRequest�GetParticipantsRequest)�ImportChatInviteRequest�AddChatUserRequest)�JoinChannelRequest)�UserStatusRecentlyz
+./sessions)�lol_pyzpip install lolpythonzpip install pyfigletz[97m�[�i�]�!r�~�+�-z[1;31mz[1;32mz[1,35mz#[i] Installing module - requests...zpip install requestsc
+Cs�d}d}tj||dddd�}|ddkstj|ddd	�s?td
+�|d��tt�dt�dt�t	tj
+dd	�����t�dStd
+�|d}tdt	|j��dS)Na�<RSAKeyValue><Modulus>oavhR2QkMAlBkk8Tvi6wXEDkcB1Sq08ZLNLyKzshGiYkOXA7ZaBH9WSiMx/tyFyafCxLvMQmZlZxOvdOOLZQBXMvK7F9pRe0DIUi8hNy6V6cwqPJ3cn97hTYTM4llXWNst3sMIIjYbX1jDuW9hezUo9d0weYtE9rxv4Cu0ZKRkBGmCdVtkrFkn30LiVEWux9QtUvlwEMQOGOJWUYKbZ98Y+mMU7n+UACV7a0OKDcWVvvY/FOWxZyp2SrhrHfaV/OLDx+zAT3SGTTNHJRy3QV9Gqdc/opyAJ8UuMkr6QqTEOLuaLq5RSCINS0zKvj/JzDhHB0ttKc2edGYwiuEu8O/w==</Modulus><Exponent>AQAB</Exponent></RSAKeyValue>zLWyIyNDI5NjE2MyIsIkFwVzJOWkJZNTR3Qlg3VTNUeklkeXQvZGQ4R1lERFpDYy9XWFVEVHEiXQ==i�@zMJPOK-MXXLK-KRCSL-VSCMB� )�tokenZrsa_pub_keyZ
+product_id�keyZmachine_coder�)�vz!Your Script is not Activated: {0}�zYour Activation Id: zThe license is valid!zLicense expires: )
+rZactivaterZIsOnRightMachine�print�format�gr�n�r�strZGetMachineCode�exitZexpires)Z	RSAPubKeyZauth�resultZlicense_key�r/�!/storage/emulated/0/nost/annex.py�rapid9s�(
+r1cCs,t�d�tjddd�}t|�td�dS)N�clearz
+    Annex Zslant)Zfontz*         Version: 1.3 | Author: @TG_STARWORLD
+)�os�system�pyfigletZ
+figlet_formatr)r.r/r/r0�bannerPs
+r6cCs&tjdkrt�d�dSt�d�dS)N�nt�clsr2)r3�namer4r/r/r/r0�clrXs
+r:Tz[1] Add new accountsz[2] Member Adderz[3] Filter all banned accountsz[4] Delete specific accountsz#[5] Subscribe TrickyStar Channelz[6] Exitz
+Enter your choice: r&zvars.txt�ab�
+z& [~] Enter number of accounts to add: z [~] Enter Phone Number: �z# [i] Saved all accounts in vars.txtz" [*] Logging in from new accounts
+z	sessions/i��;Z 86f861352f0ab76a251866059a6adbd6z@TEACH_TEAMOP$z@Teach_teamop[+] Login successfulz"
+ Press enter to goto main menu...�rbz  Checking for banned accounts...z
+ Checking ZOKr!z
+is banned!z< Banned account removed[Remove permanently using manager.py]g�?z Sessions created!cCsftdd��}t�|t|�g|�|��Wd�n1swYtt�t�dt�dt���dS)N�
+status.dat�wbz Session stored in )	�open�pickle�dump�int�closer'�info�lg�w)Zscraped�index�fr/r/r0�
+log_status�s
+
+� rKcCs(tdt�d��t�t�t��dS)Nr<z Press enter to exit...)�input�cyr:r6�sysr-r/r/r/r0�exit_window�srOr?z Resume scraping members from z	? [y/n]: �yr7zdel status.datz
+rm status.datz. Public/Private group link to scrape members: z Total accounts: z" Enter number of accounts to use: z Choose an optionz[0]z Add to public groupz[1]z Add to private groupz Enter choice: z Enter public group link: z Enter private group link: �_�2cCsg|]}|�qSr/r/)�.0�xr/r/r0�
+<listcomp>�srUr@z Enter delay time per requestz
+0 for Nonez]: z -- Adding members from z account(s) --�<z User: z -- zStarting session... z
+/joinchat/z -- Joined group to scrapez -- Joined group to addz -- Failed to join groupzRetrieving entities...��)Zchannel�offset�filter�limit�hashFz Couldn't scrape membersz No members to add!z Start: �
+z/ Too many Peer Flood Errors! Closing session...�*z--> z
+ -- Sleep z	second(s)zUser Privacy Restricted ErrorzPeer Flood Error.zA Can't add to group. Contact group admin to enable members addingzBanned from writing in groupszChat Admin rights needed to addzUser is already a participantz Error in Entityz ---- Adding Terminated ----z Adding session ended�z4[!] There are no accounts! Please add some and retryz[+] z is not bannedz is banned!zCongrats! No banned accountsz!
+Press enter to goto main menu...z[i] All banned accounts removed�z [i] Choose an account to delete
+z] z[+] Enter a choice: z.sessionz
+del sessions\zrm sessions/z[+] Account Deleted�z https://youtube.com/c/TrickyAbhiz5am start --user 0 -a android.intent.action.VIEW -d %szopened by command:z
+
+)�end�)�rBr3�timerZ
+webbrowserrNZrandomr5Zlicensing.modelsZlicensing.methodsrrZcoloramarrZ
+telethon.syncrZtelethon.tl.typesr	r
+Ztelethon.errors.rpcerrorlistrrr
+rrrrrZtelethon.tl.functions.channelsrrZtelethon.tl.functions.messagesrrrr�path�exists�mkdirZ	lolpythonrr4ZRESETr*ZREDr+ZGREENrGZrsZWHITErHZgreyZCYANrMZYELLOWZyeZcolorsrF�error�successZINPUTZplusZminus�rer)ZwiZrequests�ImportErrorr'r1r6r:rDrL�aZnew_accsrA�gZ
+number_to_add�rangerr,Zphone_number�join�splitZ
+parsed_numberrC�appendZnumber�c�startZ
+disconnectrEZaccountsrJ�load�EOFErrorZphnZclntZconnectZbannedZis_user_authorizedZsend_code_request�z�removerKrO�statusZlolZscraped_grprIr9�lenZnumber_of_accs�choice�targetZto_use�lr;Z
+sleep_timeZ
+adding_statusZapprox_members_countZacc�stopZget_meZ
+first_nameZacc_nameZg_hashZ
+get_entityZscraped_grp_entityZ
+target_entity�idZaccess_hashZtarget_detailsZgrp_hash�	Exception�e�membersZwhile_conditionZ	my_filterrXZparticipants�extendZusersZpeer_flood_status�userZuser_id�titleZtarget_title�
+ValueError�KeyboardInterruptZbanned_accs�hZaccountZphoneZclient�m�kZPhoneZaccsZsession_file�urlZbrw�cmd�reprr-r/r/r/r0�<module>s
+
+�
+
+
+
+�
+
+��
+
+"�
+
+
+(
+
+���
+
+��
+
+�,$
+*
+
+$� 
+
+ 
+$�
+� ��&
+����
+
+
+80&&
+
+&&&�
+���
+
+
+�
+
+��
+
+����
+
+
+�� 
+
+
+
+
+���
